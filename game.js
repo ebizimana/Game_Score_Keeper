c4f29b324c7faf9bc26076cb0e08d1017ef1cdb5
Bizimana <ebizimana@liberty.edu>
2018-09-27 23:41:36 -0400

    finished the reset button functionility

--- game.js
+++ game.js
@@ -5,7 +5,8 @@
 var p1Button = document.querySelector("#player1");
 var p1Display = document.querySelector("#p1");
 var p2Button = document.querySelector("#player2");
-var p2Diplay = document.querySelector("#p2");
+var p2Display = document.querySelector("#p2");
+var reset = document.querySelector("#reset")
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -26,9 +27,22 @@ p2Button.addEventListener("click", function(){
   if (!gameOver) {
     p2Score++;
     if (winningPoint === p2Score) {
-      p2Diplay.style.color = "green"
+      p2Display.style.color = "green"
       gameOver = true
     }
   }
-  p2Diplay.textContent = p2Score;
+  p2Display.textContent = p2Score;
 });
+
+reset.addEventListener("click", function(){
+  p1Score = 0
+  p2Score = 0
+
+  p1Display.textContent = 0
+  p2Display.textContent = 0
+
+  p1Display.style.color = "black"
+  p2Display.style.color = "black"
+
+  gameOver = false
+})
