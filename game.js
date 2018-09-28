41ee8f5d824170b1db57045cad23740f5791adae
Bizimana <ebizimana@liberty.edu>
2018-09-27 23:26:54 -0400

    finished the functionility of the two buttons and added color

--- game.js
+++ game.js
@@ -5,16 +5,30 @@
 var p1Button = document.querySelector("#player1");
 var p1Display = document.querySelector("#p1");
 var p2Button = document.querySelector("#player2");
-var p2Diplay = document.querySelector("#p2")
+var p2Diplay = document.querySelector("#p2");
 var p1Score = 0;
 var p2Score = 0;
+var gameOver = false;
+var winningPoint = 5
 
 p1Button.addEventListener("click", function(){
-  p1Score++;
+  if (!gameOver) {
+    p1Score++;
+    if (winningPoint === p1Score) {
+      p1Display.style.color = "green"
+      gameOver = true
+    }
+  }
   p1Display.textContent = p1Score;
 });
 
-p2Button.addEventListener("click", function() {
-  p2Score++;
-  p2Display.textContent = p2Score;
+p2Button.addEventListener("click", function(){
+  if (!gameOver) {
+    p2Score++;
+    if (winningPoint === p2Score) {
+      p2Diplay.style.color = "green"
+      gameOver = true
+    }
+  }
+  p2Diplay.textContent = p2Score;
 });
